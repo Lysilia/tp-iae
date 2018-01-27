c74e7024441f55a219356694127a7d9f6ef5b83b
perquy <perquy.leonor@gmail.com>
2018-01-27 16:35:57 +0100

    push branch tp2_perquy, non terminé

--- src/main/java/fr.elfoa/hello/jpa/C.java
+++ src/main/java/fr.elfoa/hello/jpa/C.java
@@ -3,12 +3,14 @@ package fr.elfoa.hello.jpa;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
 /**
  * @author Pierre Colomb
  */
 @Entity
+@Table(name="Plop")
 public class C {
 
     @Id
@@ -19,52 +21,36 @@ public class C {
 
     private String c_2;
 
-
-    public C(){
-    }
+    public C(){}
 
     public C(String c_1, String c_2) {
         this.c_1 = c_1;
         this.c_2 = c_2;
     }
 
-
-
     public Integer getId() {
         return id;
     }
-
-
 
     public void setId(Integer id) {
         this.id = id;
     }
 
-
-
     public String getC_1() {
         return c_1;
     }
-
-
 
     public void setC_1(String c_1) {
         this.c_1 = c_1;
     }
 
-
-
     public String getC_2() {
         return c_2;
     }
 
-
-
     public void setC_2(String c_2) {
         this.c_2 = c_2;
     }
-
-
 
     @Override
     public boolean equals(Object o) {
@@ -77,8 +63,6 @@ public class C {
                Objects.equals(c_1, c.c_1) &&
                Objects.equals(c_2, c.c_2);
     }
-
-
 
     @Override
     public int hashCode() {
--- target/generated-sources/annotations/fr/elfoa/hello/jpa/B_.java
+++ target/generated-sources/annotations/fr/elfoa/hello/jpa/B_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.2.v20151217-rNA", date="2018-01-06T22:40:01")
+@Generated(value="EclipseLink-2.6.2.v20151217-rNA", date="2018-01-08T13:09:58")
 @StaticMetamodel(B.class)
 public class B_ { 
 
